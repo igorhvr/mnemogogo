1a9f5c39ba4d7680168704f725fbebc94c33d13d
Timothy Bourke <tim@tbrk.org>
2010-02-13 13:17:29 +0100

    Need explicit import to catch encoding exceptions.

--- mnemogogo/mobile/hexcsv/HexCsv.java
+++ mnemogogo/mobile/hexcsv/HexCsv.java
@@ -24,6 +24,7 @@ import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.TimeZone;
