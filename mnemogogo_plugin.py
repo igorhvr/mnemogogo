fe0550e68f70503156f298d961b99d78f766ab17
Timothy Bourke <tim@tbrk.org>
2010-06-05 14:12:21 +0200

    Bump version number.

--- mnemogogo_plugin.py
+++ mnemogogo_plugin.py
@@ -38,7 +38,7 @@ except Exception, e:
     mnemogogo_imported_error = str(e)
 
 class MnemogogoPlugin(Plugin):
-    version = "1.1.8"
+    version = "1.2.0"
     is_locked = False
     old_overlay = None
     config_key = "mnemogogo"
